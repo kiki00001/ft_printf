c4b380f22a5020f8779e43d15271097ae3ba468c
Heesung Ahn <kiki_0001@naver.com>
2021-10-14 22:22:48 +0900

    f_printf.c ver.Norm3

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: heahn <heahn@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/10 10:39:29 by heahn             #+#    #+#             */
-/*   Updated: 2021/10/14 05:36:50 by heahn            ###   ########.fr       */
+/*   Updated: 2021/10/14 22:21:47 by heahn            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int	ft_type(va_list ap, char type)
 {
 	if (type == 'c')
-		return(ft_type_c(va_arg(ap, int)));
+		return (ft_type_c(va_arg(ap, int)));
 	else if (type == 's')
 		return (ft_type_s(va_arg(ap, char *)));
 	else if (type == 'p')
@@ -36,50 +36,30 @@ int	ft_type(va_list ap, char type)
 
 int	ft_format(va_list ap, char *format)
 {
-	int rtn;
-//	int i;
+	int	rtn;
 
 	rtn = 0;
-//	i = 0;
-	//while (format[i])
 	while (*format)
 	{
 		if (*format == '%')
 		{
 			format++;
 			rtn += ft_type(ap, *format);
-			//i++;
-			//rtn += ft_type(ap, format[i]);
 		}
 		else
-		{
-			rtn += write(1, format, 1);	
-		}//rtn += write(1, &format[i], 1);
+			rtn += write(1, format, 1);
 		format++;
-		//i++;
 	}
 	return (rtn);
 }
 
-
 int	ft_printf(const char *format, ...)
 {
-	int rtn;
+	int		rtn;
 	va_list	ap;
 
 	va_start(ap, format);
 	rtn = ft_format(ap, (char *)format);
 	va_end(ap);
 	return (rtn);
-}
-
-int main(void)
-{
-	int d;
-//	char c='c';
-//	string t ="string";
-
-	d = ft_printf("%d %d %d",42, 40, 41);
-
-	return (0);
 }
